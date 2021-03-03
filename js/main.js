8686438de20b610e8bbe0c8058666f165f73dc12
PoonamPatil99 <77851496+PoonamPatil99@users.noreply.github.com>
2021-03-03 20:16:27 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,65 @@
+function db() {
+    var name = "NA JA TU Song | Bhushan Kumar | Tanishk Bagchi | New Song 2020"
+    document.getElementById("video").poster = "./images/db3.png";
+    document.getElementById("video_heading").innerText = name
+}
+
+function n1() {
+    var name = "Baarish ( Full Video ) Neha Kakkar | Bilal Saeed | Desi Music Factory"
+    document.getElementById("video").poster = "./images/n1.png";
+    document.getElementById("video_heading").innerText = name
+}
+
+function n2() {
+    var name = "Teri Aankhon Mein Song: Divya K | Darshan R, Neha K | Pearl V Manan B | Radhika, Vinay | Bhushan K"
+    document.getElementById("video").poster = "./images/n2.png";
+    document.getElementById("video_heading").innerText = name
+}
+
+function n3() {
+    var name = "YAAD PIYA KI AANE LAGI - FULL VIDEO SONG•Divya Khosla Kumar•Neha Kakkad•Tanishk B"
+    document.getElementById("video").poster = "./images/n3.png";
+    document.getElementById("video_heading").innerText= name
+}
+
+function n4() {
+    var name = "KGF 2 Mehbooba Video Song | Yash | Nora Fatehi | Sanjay Dutt | Neha Kakkar | Prasanth N"
+    document.getElementById("video").poster = "./images/n5.png";
+    document.getElementById("video_heading").innerText = name
+}
+
+function n5() {
+    var name = "SIMMBA: Aankh Marey | Ranveer Singh, Sara Ali Khan | Tanishk Bagchi,Mika Singh,Neha Kakkar"
+    document.getElementById("video").poster = "./images/n6.png";
+    document.getElementById("video_heading").innerTextL = name
+}
+
+function n6() {
+    var name = "Oh Humsafar Lyrical | Neha Kakkar, Himansh Kohli | Tony Kakkar | Bhushan Kumar | Manoj Muntashir"
+    document.getElementById("video").poster = "./images/n10.png";
+    document.getElementById("video_heading").innerText = name
+}
+
+function n4() {
+    var name = "O saiyyaaaa jiiiiiiii"
+    document.getElementById("video").poster = "./images/n4.png";
+    document.getElementById("video_heading").innerText = name
+}
+
+
+// var n=0
+// var request = new XMLHttpRequest();
+// request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
+// request.onload = function() {
+
+   
+//     data.forEach((para) => {
+//         // document.write(chanki.director);
+//         // document.write(chanki.description);
+//         document.getElementById("q").innerText = para.director
+//         document.getElementById("qq").innerText = para.description
+        
+//     })
+    
+// }
+// request.send()
